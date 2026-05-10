579893a4879153a70ea9c22aa5a3c48e28d35336
paulopamj-png <paulo.pamj@gmail.com>
2026-05-10 01:52:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = 'estudio-aulas-v10';
+﻿const CACHE = 'estudio-aulas-v12';
 const ASSETS = ['/', '/index.html', '/icone.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
